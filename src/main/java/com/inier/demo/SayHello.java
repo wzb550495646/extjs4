705187333a1844d3935fa2226e737a7b7eaf0a2c
wzb550495646 <wzb550495646@126.com>
2021-07-13 17:23:51 +0800

    maven 配置

---
+++ src/main/java/com/inier/demo/SayHello.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2021 inier.cn All right reserved. This software is the
+ * confidential and proprietary information of inier.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with inier.cn
+ */
+package com.inier.demo;
+
+/**
+ * @author Administrator
+ * @date 2021/7/13 17:14
+ */
+public class SayHello {
+
+}
